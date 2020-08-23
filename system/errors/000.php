1e926d439622bb1c881ce91f74db451130b1cf07
Mosapi <shahral@mail.ru>
2020-08-23 11:38:41 +0300

    Create 000.php

---
+++ system/errors/000.php
@@ -0,0 +1,14 @@
+<?
+echo"
+<div id='header' class='_g'>
+	<div class=''>
+	<div>
+		<span>You have logged out of your account. We hope you will return soon.</span>
+	</div>
+	<div>
+		<a href='/'><span class='mf_t' >Main page</span></a>
+	</div>
+	</div>
+</div>
+";
+?>
