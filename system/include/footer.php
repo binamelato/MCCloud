b0b6b11d3d74fc1155b89292ea39ced0bd07896f
Mosapi <shahral@mail.ru>
2020-08-23 11:53:26 +0300

    Create footer.php

---
+++ system/include/footer.php
@@ -0,0 +1,10 @@
+<div id="pages_bottom" >
+	<div>
+		<a class='podol_n' href='/'>© MusicCloud</a> |
+		<a class='podol_n' href='/about'>About</a> |
+		<a class='podol_n' href='/blog'>blog</a> |
+		<a class='podol_n' href='/support'>help</a> |
+		<a class='podol_n' href >Donate</a> |
+		<a class='podol_n' href='/support/14'>Copyright</a>
+	</div>
+</div>
