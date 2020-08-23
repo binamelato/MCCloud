023b506267edc418b359092af9a87c12aebccbb9
Mosapi <shahral@mail.ru>
2020-08-23 11:59:14 +0300

    Create about.php

---
+++ system/include/pages/about.php
@@ -0,0 +1,44 @@
+
+<div id='header' class='_t'>
+	<div id='form_serch'></div>
+	<div>
+
+<div class='top_psf'>
+	<div class='pre_zag'>About MusicCloud</div>
+	<div class='pre_te'>Welcome, guest. You're on a resource I created. This is my personal project of musical space. It is very similar to soundcloud don't look so surprised this is. In the beginning, there was an idea if I could do it, and then it was time for a long time to work. In the end there was this resource. I'm working on it alone, so I would be very grateful if you notice a glitch, bug or something like that rather write to me on my email or leave a message about the error to me in the PM. Thanks.
+	</div>
+	<div id='bit_podtas'>
+		<div class='pr_bull'>Don't have a free account yet?</div>
+		<a class='forten' href='/'>Create account</a>
+	</div>
+	<div class='pre_te pre_pr'>
+		<div class='pre_zag'>Connect with US</div>
+		<span>For the latest news, follow us on:</span>
+		<ul id='soy_wey'>
+			<li><a href='/blog'>Our blog</a></li>
+			<!--<li><a href='http://wacebook.com/*'>Facebook</a></li>
+			<li><a href='http://twitter.com/musiccloud'>Twitter</a></li>
+			<li><a href='http://vk.com/*'>Vk</a></li>-->
+		</ul>
+	</div>
+	<div class='pre_te pre_pr' style='padding-bottom:40px;'>
+		<div class='pre_zag'>Community Support</div>
+		<span>If you need help, or have questions, find out how you can get the support you need below:</span>
+		<ul id='soy_wey'>
+			<li><a href='/support'>Help center</a></li>
+			<li><a>support@musiccloud.com</a></li>
+		</ul>
+		<span>If you want to help the resource, send your suggestions to our email:</span>
+		<ul id='soy_wey'>
+			<li><a>communty@musiccloud.com</a></li>
+		</ul>
+		<span>If you still have questions about the work of the resource, send your requests in free form to the email:</span>
+		<ul id='soy_wey'>
+			<li><a>support@musiccloud.com</a></li>
+		</ul>
+	</div>
+</div>
+	
+	</div>
+</div>
+
