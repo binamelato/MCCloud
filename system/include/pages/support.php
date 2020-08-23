ab821fb0eb9bdd8b197ea96a0b6656668c31f9fa
Mosapi <shahral@mail.ru>
2020-08-23 12:03:00 +0300

    Add files via upload

---
+++ system/include/pages/support.php
@@ -0,0 +1,85 @@
+<!--
+<div id="gkHeader" class="mess frontpage" style='background: url(/content/ie_prev/<?echo $rlogo; ?>) center;background-size:cover;'>
+<div class="mess">
+			<div id="auth_foln" style='position:absolute;left:20px;z-index:199;'><a class='spa_gh ccc-nf' href='/'>Musiccloud</a></div>
+			<div id="auth_foln" style='position:absolute;right:20px;z-index:199;'>
+<?
+if (isset($_SESSION['user_id'])) {
+				 echo"
+				 <div class='gl_m'><a href='/profile/".$_SESSION['nLog']."'><span class='spa_gh'>".$_SESSION['nLog']."</span></a></div>
+				 ";
+				}else{
+				 echo'
+				 ';
+				}
+?>
+			</div>
+		</div>
+</div>
+-->
+<div id='header' class='_t'>
+<div class="hup_hap">
+	<div class=''>
+	<div class="obve_nx _mji_" style='vertical-align:top;'>
+	<div id='que_pan'>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/1'>What is Musiccloud</a><div class='qu_cat'>get started</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/16'>General Rules</a><div class='qu_cat'>rules</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/2'>Your Display Name and Profile URL</a><div class='qu_cat'>account</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/3'>Profile image and header</a><div class='qu_cat'>account</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/6'>Messaging</a><div class='qu_cat'>account</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/10'>UNABLE TO PLAY TRACKS</a><div class='qu_cat'>tracks</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/15'>Account security</a><div class='qu_cat'>security</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/14'>Copyright</a><div class='qu_cat'>security</div></div></div>
+		<div class='q_id_1'><img class='kak_pd' src="content/sys/question.png"><div class='qu_head2'><a href='/support/13'>Bugs</a><div class='qu_cat'>bugs</div></div></div>
+
+
+	</div>
+	<div id='bit_podtul'>
+	</div>
+	</div>
+	<div class='_mji_' style='width:300px;'>
+		<div class='med_i_xx'>
+			<div class=''>
+				<ul class=''>
+					<li><a>Get started</a></li>
+						<ul class='' style='font-size:12px;'>
+							<li><a href='<?echo $fzm_host."/support/1";?>'>What is Musiccloud</a></li>
+							<li><a href='<?echo $fzm_host."/support/11";?>'>Search on Warframecloud</a></li>
+						</ul>
+					<li><a>Account</a></li>
+						<ul class='' style='font-size:12px;'>
+							<li><a href='<?echo $fzm_host."/support/2";?>'>Display Name / Profile URL</a></li>
+							<li><a href='<?echo $fzm_host."/support/3";?>'>Profile image and header</a></li>
+							<li><a href='<?echo $fzm_host."/support/6";?>'>Messaging</a></li>
+							<li><a href='<?echo $fzm_host."/support/7";?>'>Commenting</a></li>
+							<li><a href='<?echo $fzm_host."/support/8";?>'>Follower</a></li>
+						</ul>
+					<li><a>tracks</a></li>
+						<ul class='' style='font-size:12px;'>
+							<li><a href='<?echo $fzm_host."/support/5";?>'>Uploading</a></li>
+							<li><a href='<?echo $fzm_host."/support/4";?>'>Creating an Album</a></li>
+							<li><a href='<?echo $fzm_host."/support/9";?>'>Likes</a></li>
+							<li><a href='<?echo $fzm_host."/support/12";?>'>Mandachord</a></li>
+							<li><a href='<?echo $fzm_host."/support/10";?>'>Unable to play tracks</a></li>
+						</ul>
+					<li><a href='<?echo"http://".$_SERVER["HTTP_HOST"]."/support/security";?>'>Security</a></li>
+						<ul class='' style='font-size:12px;'>
+							<li><a href='<?echo $fzm_host."/support/15";?>'>Account security</a></li>
+							<li><a href='<?echo $fzm_host."/support/14";?>'>copyright</a></li>
+						</ul>
+					<li><a >Rules</a></li>
+						<ul class='' style='font-size:12px;'>
+							<li><a href='<?echo $fzm_host."/support/16";?>'>Basic rules</a></li>
+							<li><a href='<?echo $fzm_host."/support/17";?>'>Online Conduct</a></li>
+						</ul>
+					<li><a >Bugs</a></li>
+						<ul class='' style='font-size:12px;'>
+							<li><a href='<?echo $fzm_host."/support/13";?>'>bugs</a></li>
+						</ul>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</div>
